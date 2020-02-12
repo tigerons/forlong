2c6100c55c0499ea0a6d7cd1e24920173cea5771
tajger <tajger@127.0.0.1localhost>
2020-02-12 18:23:15 +0100

    Rejestracja

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'user/index'
   get 'friends/index'
   get 'friends/destroy'
   resources :friend_requests
   root to:  'pages#home'
   devise_for :users
+  match '/users',   to: 'users#index',   via: 'get'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
